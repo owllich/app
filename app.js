debd8c8bebc8e18bf99e40158c05144b5d1b5260
owllich <owllich@gmail.com>
2017-10-06 13:03:30 +0400

    7

--- app.js
+++ app.js
@@ -394,10 +394,14 @@ var app = {
 	load: function () {
 		window.load (app.update);
 		canvas.load ();
-		app.draw ();
+		app.scene.load ();
 	},
 
 	object: {},
+
+	scene: {
+		load: function () {}
+	},
 
 	update: function (event) {
 		for (let id in app.object) {
@@ -429,45 +433,83 @@ var app = {
 
 window.onload = app.load;
 
+//LOAD RESOURCES
 app.get.animations ({ 'color': 8 });
 
-app.get.images (['logo', 'minus', 'plus']);
+app.get.images (['back', 'forward', 'logo', 'minus', 'plus']);
 
-let bar = app.create.bar ({ color: '#00f', height: 10, max: 100, now: 75, width: 500, x: 100, y: 400 });
-	bar.load ();
+//CREATE OBJECTS
+app.scene.load = function () { app.scene.test (); }
 
-let bar_text = app.create.text ({ color: '#00f', size: 20, text: bar.now + ' / ' + bar.max, x: 100, y: 390 });
-	bar_text.load ();
+app.scene.test = function () {
+	app.wipe ();
 
-let minus = app.create.button ({
-	action: function () {
-		bar.add (-7);
-		bar_text.text = bar.now + ' / ' + bar.max;
-		app.draw ();
-	},
-	height: 10,
-	i: 'minus',
-	width: 10,
-	x: 610,
-	y: 400
-}).load ();
+	let bar = app.create.bar ({ color: '#00f', height: 10, max: 100, now: 75, width: 500, x: 100, y: 400 });
+		bar.load ();
 
-let plus = app.create.button ({
-	action: function () {
-		bar.add (3);
-		bar_text.text = bar.now + ' / ' + bar.max;
-		app.draw ();
-	},
-	height: 10,
-	i: 'plus',
-	width: 10,
-	x: 630,
-	y: 400 }).load ();
+	let bar_text = app.create.text ({ color: '#00f', size: 20, text: bar.now + ' / ' + bar.max, x: 100, y: 390 });
+		bar_text.load ();
 
-app.create.box ({ color: '#f00', height: 100, width: 100, x: 100, y: 100 }).load ();
+	let minus = app.create.button ({
+		action: function () {
+			bar.add (-7);
+			bar_text.text = bar.now + ' / ' + bar.max;
+			app.draw ();
+		},
+		height: 10,
+		i: 'minus',
+		width: 10,
+		x: 610,
+		y: 400
+	}).load ();
 
-app.create.sprite ({ height: 100, i: 'logo', width: 100, x: 300, y: 100 }).load ();
+	let plus = app.create.button ({
+		action: function () {
+			bar.add (3);
+			bar_text.text = bar.now + ' / ' + bar.max;
+			app.draw ();
+		},
+		height: 10,
+		i: 'plus',
+		width: 10,
+		x: 630,
+		y: 400 }).load ();
 
-app.create.sprite ({ animation: { a: app.a.color, loop: function () { return 1; }, tick: 200 }, height: 100, width: 100, x: 700, y: 100 }).load ();
+	app.create.box ({ color: '#f00', height: 100, width: 100, x: 100, y: 100 }).load ();
 
-app.create.text ({ color: '#fff', size: 24, text: 'text', width: 50, x: 400, y: 300 }).load ();
+	app.create.button ({
+		action: function () {
+			app.scene.test2 ();
+		},
+		height: 60,
+		i: 'forward',
+		width: 60,
+		x: 0.6 * window.innerWidth,
+		y: 0.5 * window.innerHeight
+	}).load ();
+
+	app.create.sprite ({ height: 100, i: 'logo', width: 100, x: 300, y: 100 }).load ();
+
+	app.create.sprite ({ animation: { a: app.a.color, loop: function () { return 1; }, tick: 200 }, height: 100, width: 100, x: 700, y: 100 }).load ();
+
+	app.create.text ({ color: '#fff', size: 24, text: 'text', width: 50, x: 400, y: 300 }).load ();
+
+	app.draw ();
+}
+
+app.scene.test2 = function () {
+	app.wipe ();
+
+	app.create.button ({
+		action: function () {
+			app.scene.test ();
+		},
+		height: 60,
+		i: 'back',
+		width: 60,
+		x: 0.6 * window.innerWidth,
+		y: 0.5 * window.innerHeight
+	}).load ();
+
+	app.draw ();
+}
